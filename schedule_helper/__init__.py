52ce616d495b42e24ee65870a03bdfcde7200c2c
TritiumQin <tritium404@hotmail.com>
2022-11-02 16:14:46 +0800

    尝试修复课程自动提示功能，并补全了注释

--- schedule_helper/__init__.py
+++ schedule_helper/__init__.py
@@ -11,39 +11,61 @@ from .gen_img import gen_week_img
 from .hourgalss import get_week_order, good_morning, ring_bell, course_reflection
 from .parse_config import Parse
 
+# “课表”命令 时间响应器
 get_table = on_command("课表")
+# 获取绝对路经
 current_path = path.abspath(__file__)
 current_dir = path.dirname(current_path)
+# 导入并解析配置
 config = rtoml.load(open(current_dir + "/config.toml", "r"))
 scheme = Parse(config)
+# 从配置中获取需要管理的 QQ 群
 group_id = scheme.settings["group_id"]
+# 获取需要上课提示的时间点
 moments = ring_bell()
+# 注入定时提醒插件
 require("nonebot_plugin_apscheduler")
 
 
 @get_table.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    """获取课表命令响应逻辑
+
+    Args:
+        bot (Bot): 当前机器人
+        event (GroupMessageEvent): 群消息事件
+        args (Message, optional): 消息命令参数. Defaults to CommandArg().
+    """
+    # 从命令参数中提取纯文本
     plain_text = args.extract_plain_text()
+    # 若不存在参数，根据当前周次自动赋值参数
     if not plain_text:
         args = get_week_order()
+    # 生成图片后，发送图片
     if gen_week_img(int(str(args))):
+        # 图片路径
         img_path = "file:///" + str(current_dir) + f"/tmp/{args}_week.jpg"
+        # 需要发送的消息
         msg = Message([
             MessageSegment(type="text", data={"text": f"你要的第{args}课表，请过目："}),
             MessageSegment(type="image", data={"file": img_path})
         ])
+        # 调用发送函数
         await bot.send(event=event, message=msg)
 
 
 @scheduler.scheduled_job('cron', hour=8, minute=0)
 async def _():
+    # 获取当前机器人
     bot = get_bot()
+    # 根据早八函数返回值决定发送消息的内容
     if good_morning():
         await bot.send_group_msg(group_id=group_id, message="糟糕，有早八")
     else:
         await bot.send_group_msg(group_id=group_id, message="无早八")
 
 
+#  注册课程提示器，每天需要注册六个
 for i in range(0, 6):
     hour = moments[i][0]
     minute = moments[i][1]
@@ -52,4 +74,4 @@ for i in range(0, 6):
     @scheduler.scheduled_job('cron', hour=hour, minute=minute)
     async def _():
         bot = get_bot()
-        await bot.send_group_msg(group_id=group_id, message=course_reflection())
+        await bot.send_group_msg(group_id=group_id, message=Message(course_reflection()))
--- schedule_helper/hourgalss.py
+++ schedule_helper/hourgalss.py
@@ -5,8 +5,10 @@ import rtoml
 
 from .parse_config import Parse
 
+# 获取绝对路经
 current_path = path.abspath(__file__)
 current_dir = path.dirname(current_path)
+# 导入配置，生成配置对象
 config = rtoml.load(open(current_dir + "/config.toml", "r"))
 scheme = Parse(config)
 
@@ -17,20 +19,29 @@ def get_week_order() -> int:
     Returns:
         int: 当前周次
     """
+    # 获取两个学期的开学时间
     semester_strings = (scheme.settings["first_half_semester"], scheme.settings["second_half_semester"])
+    # 生成当前年的开学时间，由元组类型包裹
     semester_indicator = tuple(datetime.strptime(d_str, "%Y-%m-%d") for d_str in
                                [str(datetime.now().year) + "-" + d_str for d_str in semester_strings])
     current_beginning = None
+    # 获取当前月份，来判断使用那个开学时间
     if current_month := datetime.now().month in (8, 9, 10, 11, 12, 1):
         current_beginning = semester_indicator[0]
     elif current_month in (2, 3, 4, 5, 6, 7):
         current_beginning = semester_indicator[1]
+    # 计算周数
     today = datetime.today()
     diff_days = (today - current_beginning).days
     return diff_days // 7 + 1
 
 
 def today_in_week() -> str:
+    """获取今天星期几
+
+    Returns:
+        str: "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" 中的其一
+    """
     current_day = datetime.today()
     current_week_day = current_day.weekday()
     week_tuple = ("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday")
@@ -39,6 +50,11 @@ def today_in_week() -> str:
 
 
 def good_morning() -> bool:
+    """早八函数，判断每天是否存在第一节课，来决定返回值
+
+    Returns:
+        bool: 早上是否有课
+    """
     index_str = today_in_week()
     current_day_source = scheme.get_day(get_week_order(), index_str)
     # 检查器，若存在第一节课则返回其内容，不存在返回 None
@@ -49,19 +65,30 @@ def good_morning() -> bool:
 
 
 def ring_bell() -> tuple:
+    """获取上课时间点
+
+    Returns:
+        tuple: 形式如同 ((<int>,<int>)…)，每个被包裹的元组里依次是小时与分钟
+    """
     time_list = [scheme.time_set[f"schedule_{d}"] for d in range(1, 7)]
     time_convert = [(moment.hour, moment.minute) for moment in time_list]
     return tuple(time_convert)
 
 
-def course_reflection() -> tuple:
+def course_reflection() -> str:
+    """根据当前时间返回即将开始的课程
+
+    Returns:
+        str: 课程信息，包括课程名与教室
+    """
     hour = datetime.now().hour
     minute = datetime.now().minute
     now_tuple = (hour, minute)
     ring_times = ring_bell()
     course_hour_point = course_minute_point = None
     for moment in ring_times:
-        if moment[0] < now_tuple[0]:
+        #  只需要判断小时
+        if moment[0] == now_tuple[0]:
             course_hour_point = moment[0]
             course_minute_point = moment[1]
     if course_minute_point < 10:
@@ -77,5 +104,10 @@ def course_reflection() -> tuple:
             index_str = "class_" + str(i + 1)
     week_order = get_week_order()
     week_day_index = today_in_week()
-    today_cource = scheme.get_day(order=week_order, day=week_day_index)
-    return today_cource.get(index_str, None)
+    today_course = scheme.get_day(order=week_order, day=week_day_index)
+    # 初始化课程信息
+    course_info = ""
+    # 获取当前时间点课程
+    if current_course := today_course.get(index_str, None):
+        course_info = str(current_course.get("course")) + "\n" + str(current_course.get("room"))
+    return course_info
